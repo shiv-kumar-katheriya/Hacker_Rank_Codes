8a7c57ef0f0496d540478f51cc065e0415cc6fee
shiv-kumar-katheriya <59054892+shiv-kumar-katheriya@users.noreply.github.com>
2020-01-17 23:12:21 +0530

    Update collections.Counter().py

--- Python/collections.Counter().py
+++ Python/collections.Counter().py
@@ -4,7 +4,18 @@ size =  list(map(int,input().split()))
 no = int(input())
 size = Counter(size)
 p = list(size.keys())
-print(p)
-##for i in range(no):
-##    z = list(map(int,input()))
+m = list(size.values())
+count = 0
 
+for i in range(no):
+    z = list(map(int,input().split()))
+    if z[0] in p:
+        q = p.index(z[0])
+        if m[q] != 0:
+            count +=  z[1]
+            m[q] -= 1
+    else :
+        z = []
+        continue
+    
+print(count)
